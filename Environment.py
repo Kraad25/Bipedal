00bbacace5bf531bad14f7f87a27dc9d4baefd70
Krishna <krishnaprasaad.r12@gmail.com>
2025-06-03 15:31:28 +0530

    Initial Commit

---
+++ Environment.py
@@ -0,0 +1,18 @@
+import gymnasium as gym
+import pygame
+
+class BipedalWalkerEnv(gym.Env):
+    def __call__(self, *args, **kwds):
+        pass
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        pass
+    
+    def render(self, mode='human'):
+        pass
+
+    def close(self):
+        pass
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Bipedal
-
+    A bipedal Robot that is taught how to stand and walk on its 2 legs. It has 4 joints, 2 in each leg
+    and a main body/hull.
